cf6064aca83189ffb91a183079ad6f7ac26ba503
MLombriz <mj.labo.91@gmail.com>
2021-06-13 11:29:34 -0300

    start taller-shop

--- taller-shop/src/App.js
+++ taller-shop/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Primera prueba de React en Taller Urbano App
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
